8a9e9a26a6e191500fcce8cd2b412af8b466273b
HarshaKundlikar <hkundlikar@gmail.com>
2024-10-22 16:34:12 +0530

    Git try

--- src/Sept_one/Lab001.java
+++ src/Sept_one/Lab001.java
@@ -1,4 +1,7 @@
 package Sept_one;
 
 public class Lab001 {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
 }
--- src/Sept_one/Lab004.java
+++ src/Sept_one/Lab004.java
@@ -1,4 +1,12 @@
 package Sept_one;
 
+
+import java.sql.SQLOutput;
+
 public class Lab004 {
+    //System.out.println("checking git");
+    public static void main(String args[]){
+        System.out.println("checking git");
+    }
+
 }
